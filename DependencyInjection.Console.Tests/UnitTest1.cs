38415afc255122ffbaaa2f12f6860f6598ed241d
a2wd <github@a2wd.com>
2017-07-18 16:55:27 +0100

    Added gitignore and project skeleton

---
+++ DependencyInjection.Console.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyInjection.Console.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
